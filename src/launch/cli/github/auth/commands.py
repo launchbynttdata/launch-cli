baf7578c7ef2466e3bcf495827265003209ef83c
Aarti Joshi <aarti.joshi@nttdata.com>
2024-06-27 17:29:03 -0400

    Launch Command to create token using github app

---
+++ src/launch/cli/github/auth/commands.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import click
+
+from launch.github.generate_github_token import get_token
+
+
+@click.command()
+@click.option(
+    "--application_id_parameter_name",
+    required=True,
+    help=f"Name of the parameter from AWS System Manager parameter store that contains the application id of the GitHub App.",
+)
+@click.option(
+    "--installation_id_parameter_name",
+    required=True,
+    help="Name of the parameter from AWS System Manager parameter store that contains the installation id of the GitHub App.",
+)
+@click.option(
+    "--signing_cert_secret_name",
+    required=True,
+    help="Name of the parameter from AWS System Manager parameter store that contains the name of the secret from AWS Secrets Manager that has the signing certificate of the GitHub App.",
+)
+def application(
+    application_id_parameter_name: str,
+    installation_id_parameter_name: str,
+    signing_cert_secret_name: str,
+):
+    # signing_cert_secret = Path(signing_cert_secret)  # Convert string to Path object
+    token = get_token(
+        application_id_parameter_name=application_id_parameter_name,
+        installation_id_parameter_name=installation_id_parameter_name,
+        signing_cert_secret_name=signing_cert_secret_name,
+    )
+    print(f"Generated GitHub Token: {token}")
