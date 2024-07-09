a725bf79d6f70e4c179f95b53d03cdd07dfe2408
Aaron Christian <aaron.christian@nttdata.com>
2024-07-09 18:24:15 +0000

    uncommenting from test

--- src/launch/cli/github/auth/__init__.py
+++ src/launch/cli/github/auth/__init__.py
@@ -1,11 +1,11 @@
-# import click
+import click
 
-# from .commands import application
-
-
-# @click.group(name="auth")
-# def auth_group():
-#     """Command family for dealing with generation of tokens using GitHub App."""
+from .commands import application
 
 
-# auth_group.add_command(application)
+@click.group(name="auth")
+def auth_group():
+    """Command family for dealing with generation of tokens using GitHub App."""
+
+
+auth_group.add_command(application)
