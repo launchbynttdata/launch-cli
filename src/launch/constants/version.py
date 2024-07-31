b8c2a41b3425748efa8b9512b8e65da45963987b
nobody@nttdata.com <aaron.christian@nttdata.com>
2024-07-31 17:45:58 +0000

    bump version

--- src/launch/constants/version.py
+++ src/launch/constants/version.py
@@ -1,9 +1,5 @@
 from semver import Version
 
-<<<<<<< HEAD
 VERSION = "0.9.3"
-=======
-VERSION = "0.9.2"
->>>>>>> 9ec9a1a9ab92cbbe5fd60fd394c046ac1a55e025
 
 SEMANTIC_VERSION = Version.parse(VERSION)
