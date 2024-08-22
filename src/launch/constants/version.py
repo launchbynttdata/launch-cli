0fab18b96e1718f1b8f6efd36ba54b2e2c1d21e3
aaron-christian-nttd <aaron.christian@nttdata.com>
2024-08-22 13:14:18 +0000

    bounce

--- src/launch/constants/version.py
+++ src/launch/constants/version.py
@@ -1,20 +1,5 @@
 from semver import Version
 
-from launch.lib.common.utilities import read_toml_version
-
-VERSION = read_toml_version()
+VERSION = "0.9.6"
 
 SEMANTIC_VERSION = Version.parse(VERSION)
-
-
-def read_toml_version():
-    with open("./../../../pyproject.toml", "r") as file:
-        lines = file.readlines()
-
-    version = None
-    for line in lines:
-        if line.startswith("version"):
-            version = line.split("=")[1].strip().strip('"')
-            break
-
-    return version
