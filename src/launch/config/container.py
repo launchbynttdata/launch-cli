8857327dd6a966887902a4108b362a35d665cd47
nobody <nobody@nttdata.com>
2024-09-08 20:11:55 +0000

    removing readFile

--- src/launch/config/container.py
+++ src/launch/config/container.py
@@ -1,5 +1,4 @@
 from launch.env import override_default
-from launch.lib.automation.environment.functions import readFile
 
 CONTAINER_IMAGE_NAME = override_default(
     key_name="CONTAINER_IMAGE_NAME",
@@ -8,7 +7,7 @@ CONTAINER_IMAGE_NAME = override_default(
 
 CONTAINER_IMAGE_VERSION = override_default(
     key_name="CONTAINER_IMAGE_VERSION",
-    default=readFile("CONTAINER_IMAGE_VERSION"),
+    default=None,
 )
 
 CONTAINER_REGISTRY = override_default(
