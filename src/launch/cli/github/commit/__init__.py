f701b7fff8f0fe55344916aa03802e7c2c94ccd1
Chris Taylor <chris11.taylor@nttdata.com>
2024-09-12 15:13:55 -0500

    Fix command family help description

--- src/launch/cli/github/commit/__init__.py
+++ src/launch/cli/github/commit/__init__.py
@@ -5,7 +5,7 @@ from .status import status_group
 
 @click.group(name="commit")
 def commit_group():
-    """Command family for dealing with GitHub webhooks."""
+    """Command family for dealing with GitHub commits, including commit status."""
 
 
 commit_group.add_command(status_group)
