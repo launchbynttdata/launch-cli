9e7d348bd90077d1ce1ddd5337f015d2a9694d13
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-10-08 09:40:52 -0600

    Feat: add tests for launc.cli.service.build

--- test/unit/cli/service/build/test_build.py
+++ test/unit/cli/service/build/test_build.py
@@ -1,6 +1,39 @@
-import pytest
+from launch.cli.service.build import build
 
 
-@pytest.mark.skip("TODO: Ticket: tbd XXXXX")
-def test_build():
-    pass
+class TestBuild:
+    def test_build_ok(self, cli_runner, mocker):
+        result = cli_runner.invoke(
+            build,
+            ["--provider", "aws"],
+        )
+        assert not result.exception
+        assert result.exit_code == 0
+
+    def test_build_dry_run(self, cli_runner, mocker):
+        result = cli_runner.invoke(
+            build,
+            ["--provider", "aws", "--dry-run"],
+        )
+
+        # Debugging outputs
+        print(f"Output: {result.output}")
+        print(f"Exit Code: {result.exit_code}")
+        print(f"Exception: {result.exception}")
+
+        assert not result.exception
+        assert result.exit_code == 0
+
+    def test_build_invalid_configuration(self, cli_runner, mocker):
+        result = cli_runner.invoke(
+            build,
+            ["--provider", "aws", "--url", "invalid-url"],
+        )
+
+        # Debugging outputs
+        print(f"Output: {result.output}")
+        print(f"Exit Code: {result.exit_code}")
+        print(f"Exception: {result.exception}")
+
+        assert result.exception
+        assert result.exit_code == 1
