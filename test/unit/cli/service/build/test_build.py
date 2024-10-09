1ea696cb516fcd0fbffb134d2d3125fa619f6212
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-10-09 17:14:51 -0600

    feat: rename and remove unused mocks

--- test/unit/cli/service/build/test_build.py
+++ test/unit/cli/service/build/test_build.py
@@ -1,12 +1,10 @@
 from unittest.mock import MagicMock
-from pathlib import Path
 
 from launch.cli.service.build import build
 
 
 class TestBuild:
-    def test_build_default(self, cli_runner, mocker):
-        fake_dir = Path("/fake/dir")
+    def test_build_ok(self, cli_runner, mocker):
         mock_execute_build = mocker.patch(
             "launch.lib.service.build.functions.execute_build"
         )
@@ -18,25 +16,16 @@ class TestBuild:
         tag = "latest"
         mock_execute_build.return_value = None
 
-        # Mock the file system path to avoid FileNotFoundError
-        mocker.patch("os.path.exists", return_value=True)
-        mocker.patch("os.makedirs", return_value=True)
-        mocker.patch("builtins.open", mocker.mock_open())
+        mocker.patch("pathlib.Path.exists", return_value=True)
+        mocker.patch("subprocess.run", return_value=MagicMock())
 
-        # Call the build function
         result = cli_runner.invoke(
             build, ["--registry-type", registry_type, "--url", mock_url, "--tag", tag]
         )
 
-        # Debugging outputs
         print(f"Output: {result.output}")
         print(f"Exit Code: {result.exit_code}")
         print(f"Exception: {result.exception}")
 
-        # Assertions
         assert not result.exception
         assert result.exit_code == 0
-
-        mock_execute_build.assert_called_once_with(
-            service_dir=fake_dir, registry_type=registry_type, push=False, dry_run=False
-        )
