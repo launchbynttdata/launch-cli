6ddff1544348b6ef240cc0e3459685c97d7743b4
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-10-09 15:31:33 -0600

    feat: add test with mocked launchconfig

--- test/unit/cli/service/build/test_build.py
+++ test/unit/cli/service/build/test_build.py
@@ -1,5 +1,7 @@
 from launch.cli.service.build import build
 
+from unittest.mock import MagicMock
+
 
 class TestBuild:
     def test_build_ok(self, cli_runner, mocker):
@@ -27,3 +29,19 @@ class TestBuild:
 
         assert result.exception
         assert result.exit_code == 1
+
+    def test_build_with_mocked_launchconfig_name(self, cli_runner, mocker):
+        mock_execute_build = mocker.patch("launch.lib.service.build.execute_build")
+        mock_clone = mocker.patch("git.Repo.clone_from")
+        mock_url = "https://github.com/fakerepo.git"
+        mock_repo = MagicMock()
+        mock_clone.return_value = mock_repo
+        mock_execute_build.return_value = None
+
+        result = cli_runner.invoke(
+            build,
+            ["--provider", "aws", "--url", mock_url],
+        )
+
+        assert not result.exception
+        assert result.exit_code == 0
