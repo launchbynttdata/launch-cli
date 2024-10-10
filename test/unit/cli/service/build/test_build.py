3861aba3951ef2b42a439d5a95dcb07155916dae
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-10-10 07:27:35 -0600

    add no url and dockerfile builds

--- test/unit/cli/service/build/test_build.py
+++ test/unit/cli/service/build/test_build.py
@@ -23,9 +23,42 @@ class TestBuild:
             build, ["--registry-type", registry_type, "--url", mock_url, "--tag", tag]
         )
 
-        print(f"Output: {result.output}")
-        print(f"Exit Code: {result.exit_code}")
-        print(f"Exception: {result.exception}")
+        assert not result.exception
+        assert result.exit_code == 0
+
+    def test_build_no_url(self, cli_runner, mocker):
+        mocker.patch("launch.constants.launchconfig.LAUNCHCONFIG_NAME", "launchconfig")
+        mock_execute_build = mocker.patch(
+            "launch.lib.service.build.functions.execute_build"
+        )
+        mock_clone = mocker.patch("git.Repo.clone_from")
+        mock_repo = MagicMock()
+        mock_clone.return_value = mock_repo
+        registry_type = "docker"
+        tag = "latest"
+        mock_execute_build.return_value = None
+
+        mocker.patch("pathlib.Path.exists", return_value=True)
+        mocker.patch("subprocess.run", return_value=MagicMock())
+
+        result = cli_runner.invoke(
+            build, ["--registry-type", registry_type, "--tag", tag]
+        )
+
+        assert not result.exception
+        assert result.exit_code == 0
+
+    def test_build_dockerfile(self, cli_runner, mocker):
+        mocker.patch("launch.config.common.DOCKER_FILE_NAME", "Dockerfile")
+        registry_type = "docker"
+        tag = "latest"
+
+        mocker.patch("pathlib.Path.exists", return_value=True)
+        mocker.patch("subprocess.run", return_value=MagicMock())
+
+        result = cli_runner.invoke(
+            build, ["--registry-type", registry_type, "--tag", tag]
+        )
 
         assert not result.exception
         assert result.exit_code == 0
