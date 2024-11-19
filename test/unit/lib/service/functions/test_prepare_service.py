a2b083649fc0cf3868d20c948a3f920f412274f6
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-11-19 11:54:44 -0600

    Added test functions to validate prepare_service

--- test/unit/lib/service/functions/test_prepare_service.py
+++ test/unit/lib/service/functions/test_prepare_service.py
@@ -1,6 +1,57 @@
+import json
+from pathlib import Path
+
 import pytest
 
+from launch.constants.launchconfig import LAUNCHCONFIG_NAME
+from launch.lib.service.functions import prepare_service
 
-@pytest.mark.skip("TODO: Ticket: tbd XXXXX")
-def test_prepare_service():
-    pass
+
+def test_prepare_service(mocker):
+    # Mock dependencies
+    mock_get_github_instance = mocker.patch(
+        "launch.lib.service.functions.get_github_instance"
+    )
+    mock_path_exists = mocker.patch("launch.lib.service.functions.Path.exists")
+    mock_path_read_text = mocker.patch("launch.lib.service.functions.Path.read_text")
+
+    # Test dry run with in_file
+    mock_file = mocker.mock_open(read_data=json.dumps({"key": "value"}))
+    mocker.patch("builtins.open", mock_file)
+    mocker.patch("json.load", return_value={"key": "value"})
+    with open("mock_file") as f:
+        input_data, service_path, repository, g = prepare_service(
+            name="test_service", in_file=f, dry_run=True
+        )
+    assert input_data == {
+        "key": "value",
+        "skeleton": {
+            "url": "https://github.com/launchbynttdata/lcaf-template-terragrunt.git",
+            "tag": "main",
+        },
+    }
+    assert service_path == f"{Path.cwd()}/test_service"
+    assert repository is None
+    assert g == mock_get_github_instance.return_value
+
+    # Test dry run without in_file but with LAUNCHCONFIG_NAME
+    mock_path_exists.return_value = True
+    mock_path_read_text.return_value = json.dumps({"key": "value"})
+    input_data, service_path, repository, g = prepare_service(
+        name="test_service", in_file=None, dry_run=True
+    )
+    assert input_data == {
+        "key": "value",
+        "skeleton": {
+            "url": "https://github.com/launchbynttdata/lcaf-template-terragrunt.git",
+            "tag": "main",
+        },
+    }
+    assert service_path == f"{Path.cwd()}"
+    assert repository is None
+    assert g == mock_get_github_instance.return_value
+
+    # Test no in_file and no LAUNCHCONFIG_NAME
+    mock_path_exists.return_value = False
+    with pytest.raises(SystemExit):
+        prepare_service(name="test_service", in_file=None, dry_run=True)
