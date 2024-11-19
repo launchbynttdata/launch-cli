b03da9e5b9fa785a92bdddbaff82302d3c37ea3d
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-11-19 15:19:42 -0600

    add test for readFile

--- test/unit/lib/automation/environment/functions/test_readFile.py
+++ test/unit/lib/automation/environment/functions/test_readFile.py
@@ -1,6 +1,29 @@
+from unittest import mock
+
 import pytest
 
+from launch.lib.automation.environment.functions import readFile
 
-@pytest.mark.skip("TODO: Ticket: tbd XXXXX")
-def test_readfile():
-    pass
+
+def test_readFile_key_found(mocker):
+    mock_file_content = 'export TEST_KEY="test_value"\n'
+    mocker.patch("builtins.open", mock.mock_open(read_data=mock_file_content))
+
+    result = readFile("TEST_KEY", "dummy_path")
+    assert result == "test_value"
+
+
+def test_readFile_key_not_found(mocker):
+    mock_file_content = 'export ANOTHER_KEY="another_value"\n'
+    mocker.patch("builtins.open", mock.mock_open(read_data=mock_file_content))
+
+    result = readFile("TEST_KEY", "dummy_path")
+    assert result is None
+
+
+def test_readFile_file_not_found(mocker):
+    mocker.patch("builtins.open", side_effect=FileNotFoundError)
+    mocker.patch("click.secho")  # Mock click.secho to avoid actual print in test
+
+    result = readFile("TEST_KEY", "dummy_path")
+    assert result is None
