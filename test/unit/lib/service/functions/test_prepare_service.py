8c3ee1a25025a6cb7b5ccb8b5db2c4226482dbd8
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-11-19 12:49:24 -0600

    add mock_click_secho assert

--- test/unit/lib/service/functions/test_prepare_service.py
+++ test/unit/lib/service/functions/test_prepare_service.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pytest
 
+from launch.config.common import BUILD_TEMP_DIR_PATH
 from launch.constants.launchconfig import LAUNCHCONFIG_NAME
 from launch.lib.service.functions import prepare_service
 
@@ -14,6 +15,7 @@ def test_prepare_service(mocker):
     )
     mock_path_exists = mocker.patch("launch.lib.service.functions.Path.exists")
     mock_path_read_text = mocker.patch("launch.lib.service.functions.Path.read_text")
+    mock_click_secho = mocker.patch("launch.lib.service.functions.click.secho")
 
     # Test dry run with in_file
     mock_file = mocker.mock_open(read_data=json.dumps({"key": "value"}))
@@ -33,6 +35,10 @@ def test_prepare_service(mocker):
     assert service_path == f"{Path.cwd()}/test_service"
     assert repository is None
     assert g == mock_get_github_instance.return_value
+    mock_click_secho.assert_called_with(
+        f"[DRYRUN] Would have removed the following directory: {BUILD_TEMP_DIR_PATH=}",
+        fg="yellow",
+    )
 
     # Test dry run without in_file but with LAUNCHCONFIG_NAME
     mock_path_exists.return_value = True
@@ -50,8 +56,16 @@ def test_prepare_service(mocker):
     assert service_path == f"{Path.cwd()}"
     assert repository is None
     assert g == mock_get_github_instance.return_value
+    mock_click_secho.assert_called_with(
+        f"[DRYRUN] Would have removed the following directory: {BUILD_TEMP_DIR_PATH=}",
+        fg="yellow",
+    )
 
     # Test no in_file and no LAUNCHCONFIG_NAME
     mock_path_exists.return_value = False
     with pytest.raises(SystemExit):
         prepare_service(name="test_service", in_file=None, dry_run=True)
+    mock_click_secho.assert_called_with(
+        f"No --in-file supplied and could not find {LAUNCHCONFIG_NAME} in the current directory. Exiting...",
+        fg="red",
+    )
