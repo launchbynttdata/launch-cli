279856d1d02f1c556c28b90310d4597110ae7e46
Ankit Renduchintala <ankit.renduchintala@nttdata.com>
2024-11-19 12:11:02 -0500

    268: adding test cases

--- test/unit/lib/service/template/functions/test_get_value_by_path.py
+++ test/unit/lib/service/template/functions/test_get_value_by_path.py
@@ -1,6 +1,56 @@
 import pytest
 
+from unittest import mock
+from launch.lib.service.template.functions import get_value_by_path
 
-@pytest.mark.skip("TODO: Ticket: tbd XXXXX")
-def test_get_value_by_path():
-    pass
+# @pytest.mark.skip("TODO: Ticket: tbd XXXXX")
+# def test_get_value_by_path():
+#     pass
+
+def test_get_value_by_path_success():
+    data = {
+        'a': {
+            'b': {
+                'c': 'value'
+            }
+        }
+    }
+    path = 'a/b/c'
+    result = get_value_by_path(data, path)
+    assert result == 'value'
+
+def test_get_value_by_path_failure():
+    data = {
+        'a': {
+            'b': {
+                'c': 'value'
+            }
+        }
+    }
+    path = 'a.b.d'
+    result = get_value_by_path(data, path)
+    assert result is None
+
+def test_get_value_by_path_empty_path():
+    data = {
+        'a': {
+            'b': {
+                'c': 'value'
+            }
+        }
+    }
+    path = ''
+    result = get_value_by_path(data, path)
+    assert result is None
+
+def test_get_value_by_path_nonexistent_key():
+    data = {
+        'a': {
+            'b': {
+                'c': 'value'
+            }
+        }
+    }
+    path = 'a.x.c'
+    result = get_value_by_path(data, path)
+    assert result is None
