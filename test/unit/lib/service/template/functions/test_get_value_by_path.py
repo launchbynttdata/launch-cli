5cc46ac73418e55a118a54e16b4c59b47a1b49b3
Ankit Renduchintala <ankit.renduchintala@nttdata.com>
2024-11-20 16:22:28 -0500

    Patch/268 value by path (#48)
    * 268: adding test cases
    
    * removing unnecessary comments
    
    * updating development installments steps for windows 11
    
    * Fixing development installation steps
    
    * refactoring test data object and fixing test cases

--- test/unit/lib/service/template/functions/test_get_value_by_path.py
+++ test/unit/lib/service/template/functions/test_get_value_by_path.py
@@ -1,6 +1,24 @@
 import pytest
 
+from launch.lib.service.template.functions import get_value_by_path
+from test.conftest import fakeData_forPath
 
-@pytest.mark.skip("TODO: Ticket: tbd XXXXX")
-def test_get_value_by_path():
-    pass
+def test_get_value_by_path_success(fakeData_forPath):
+    path = 'a/b/c'
+    result = get_value_by_path(fakeData_forPath, path)
+    assert result == 'value'
+
+def test_get_value_by_path_failure(fakeData_forPath):
+    path = 'a.b.d'
+    result = get_value_by_path(fakeData_forPath, path)
+    assert result is None
+
+def test_get_value_by_path_empty_path(fakeData_forPath):
+    path = ''
+    result = get_value_by_path(fakeData_forPath, path)
+    assert result is None
+
+def test_get_value_by_path_nonexistent_key(fakeData_forPath):
+    path = 'a/1/c'
+    result = get_value_by_path(fakeData_forPath, path)
+    assert result is None
