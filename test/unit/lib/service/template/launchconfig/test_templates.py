7d92a1e1b8f37be8822b328e34a43c1d6d6736c0
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-11-21 08:29:23 -0600

    add test for templates

--- test/unit/lib/service/template/launchconfig/test_templates.py
+++ test/unit/lib/service/template/launchconfig/test_templates.py
@@ -1,6 +1,38 @@
+import os
+import shutil
+from pathlib import Path
+from unittest.mock import MagicMock
+
 import pytest
 
+from launch.enums.launchconfig import LAUNCHCONFIG_KEYS
+from launch.lib.service.template.launchconfig import LaunchConfigTemplate
 
-@pytest.mark.skip("TODO: Ticket: tbd XXXXX")
-def test_templates():
-    pass
+
+def test_templates(mocker, fakedata):
+    mocker.patch("os.makedirs")
+    mocker.patch("shutil.copy")
+
+    value = {"templates": fakedata["templates"]}
+    current_path = Path("/absolute/path/to/current")
+    dest_base = Path("/absolute/path/to/")
+
+    launch_config = LaunchConfigTemplate(dry_run=False)
+    launch_config.templates(value, current_path, dest_base)
+
+    os.makedirs.assert_called_with(
+        current_path.joinpath("templates/my_template"), exist_ok=True
+    )
+
+    print(dest_base.joinpath("./current/templates/my_template/readme.yaml"))
+    print(value["templates"]["my_template"]["readme"])
+
+    # Ensure value dictionary updates correctly
+    assert value["templates"]["my_template"]["readme"] == (
+        "./current/templates/my_template/readme.yaml"
+    )
+    assert value["templates"]["my_template"]["config"] == (
+        "./current/templates/my_template/config.yaml"
+    )
+
+    assert shutil.copy.call_count == 2
