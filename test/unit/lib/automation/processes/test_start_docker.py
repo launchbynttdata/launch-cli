0c55c1ec9840b9bdf9943ce3fafe6a92d1d5d36b
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-11-21 08:57:33 -0600

    add missing asserts to docker test

--- test/unit/lib/automation/processes/test_start_docker.py
+++ test/unit/lib/automation/processes/test_start_docker.py
@@ -5,27 +5,21 @@ import pytest
 from launch.lib.automation.processes.functions import start_docker
 
 
-def mock_dependencies(mocker, is_running=False):
-    mocker.patch(
-        "launch.lib.automation.processes.functions.is_docker_running",
-        return_value=is_running,
-    )
-    mocker.patch("subprocess.Popen")
-    mocker.patch("launch.lib.service.functions.click.secho")
-    mocker.patch("time.sleep")
+def test_start_docker_dry_run(mocker):
+    mock_click_secho = mocker.patch("launch.lib.service.functions.click.secho")
+    start_docker(dry_run=True)
+    mock_click_secho.assert_called_once_with(f"Starting docker if not running")
 
 
 def test_start_docker_dry_run(mocker):
-    mock_dependencies(mocker, is_running=False)
+    mock_click_secho = mocker.patch("launch.lib.service.functions.click.secho")
     start_docker(dry_run=True)
-
-
-def test_start_docker_non_dry_run(mocker):
-    mock_dependencies(mocker, is_running=False)
-    mocker.patch("subprocess.Popen", return_value=mocker.MagicMock())
-    start_docker(dry_run=False)
+    mock_click_secho.assert_called_with(
+        f"[DRYRUN] Would have started docker daemon", fg="yellow"
+    )
 
 
 def test_start_docker_already_running(mocker):
-    mock_dependencies(mocker, is_running=True)
+    mock_click_secho = mocker.patch("launch.lib.service.functions.click.secho")
     start_docker(dry_run=False)
+    mock_click_secho.assert_called_once_with(f"Starting docker if not running")
