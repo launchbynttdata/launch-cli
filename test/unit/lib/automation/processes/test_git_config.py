cf1f838bf851858962801788ca59fe355023b82f
dustin-ekema-nttd <267038@nttdata.com>
2024-11-21 10:55:59 -0500

    Added test_is_docker_running tests and changed test_git_config asserts

--- test/unit/lib/automation/processes/test_git_config.py
+++ test/unit/lib/automation/processes/test_git_config.py
@@ -1,25 +1,28 @@
 import subprocess
+import unittest
 from unittest.mock import patch
 import pytest
 from launch.lib.automation.processes.functions import git_config
 
-def test_git_config_dry_run():
-    with patch("click.secho") as mock_secho, patch("subprocess.run") as mock_run:
-        git_config(dry_run=True)
-        mock_secho.assert_any_call(f"[DRYRUN] Would have ran subprocess: git config", fg="yellow")
-        mock_run.assert_not_called()
+class TestGitConfig(unittest.TestCase):
 
-def test_git_config_success():
-    with patch("click.secho") as mock_secho, patch("subprocess.run") as mock_run:
-        git_config(dry_run=False)
-        mock_secho.assert_any_call(f"Running make git config")
-        assert mock_run.call_count == 2
-        mock_run.assert_any_call(["git", "config", "--global", "user.name", "nobody"], check=True)
-        mock_run.assert_any_call(["git", "config", "--global", "user.email", "nobody@nttdata.com"], check=True)
+    def test_git_config_dry_run(self):
+        with patch("click.secho") as mock_secho, patch("subprocess.run") as mock_run:
+            git_config(dry_run=True)
+            mock_secho.assert_any_call(f"[DRYRUN] Would have ran subprocess: git config", fg="yellow")
+            mock_run.assert_not_called()
 
-def test_git_config_failure():
-    with patch("subprocess.run") as mock_run:
-        mock_run.side_effect = subprocess.CalledProcessError(1, "git config")
-        with pytest.raises(RuntimeError) as exc_info:
+    def test_git_config_success(self):
+        with patch("click.secho") as mock_secho, patch("subprocess.run") as mock_run:
             git_config(dry_run=False)
-        assert "An error occurred:" in str(exc_info.value)
+            mock_secho.assert_any_call(f"Running make git config")
+            self.assertEqual(mock_run.call_count,2)
+            mock_run.assert_any_call(["git", "config", "--global", "user.name", "nobody"], check=True)
+            mock_run.assert_any_call(["git", "config", "--global", "user.email", "nobody@nttdata.com"], check=True)
+
+    def test_git_config_failure(self):
+        with patch("subprocess.run") as mock_run:
+            mock_run.side_effect = subprocess.CalledProcessError(1, "git config")
+            with pytest.raises(RuntimeError) as exc_info:
+                git_config(dry_run=False)
+            self.assertIn("An error occurred:",str(exc_info.value))
--- test/unit/lib/automation/processes/test_is_docker_running.py
+++ test/unit/lib/automation/processes/test_is_docker_running.py
@@ -1,6 +1,30 @@
 import pytest
+import unittest
+from unittest.mock import patch, MagicMock
+import subprocess
+from launch.lib.automation.processes.functions import is_docker_running
 
+class TestIsDockerRunning(unittest.TestCase):
 
-@pytest.mark.skip("TODO: Ticket: tbd XXXXX")
-def test_is_docker_running():
-    pass
+    def test_is_docker_running_success(self):
+        with(patch("subprocess.run")) as mock_run:
+            mock_run.return_value = MagicMock(returncode=0)
+        
+            result = is_docker_running()
+        
+            mock_run.assert_called_once_with(
+                ["docker", "ps"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            self.assertTrue(result)
+
+    
+    def test_is_docker_running_failure(self):
+        with(patch("subprocess.run")) as mock_run:
+            mock_run.side_effect = subprocess.CalledProcessError(1, ["docker", "ps"])
+            
+            result = is_docker_running()
+            
+            mock_run.assert_called_once_with(
+                ["docker", "ps"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            self.assertFalse(result)
