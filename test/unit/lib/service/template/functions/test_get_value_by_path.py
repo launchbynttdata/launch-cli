e92899085bf3f9522f7e18814d2b39fe74d49d80
Ankit Renduchintala <ankit.renduchintala@nttdata.com>
2024-11-22 16:52:15 -0500

    refactoring test data to json files and updating test functions

--- test/unit/lib/service/template/functions/test_get_value_by_path.py
+++ test/unit/lib/service/template/functions/test_get_value_by_path.py
@@ -1,24 +1,27 @@
 import pytest
 
 from launch.lib.service.template.functions import get_value_by_path
-from test.conftest import fakeData_forPath
+from test.conftest import fakeData_forLibServiceTemplateFunction as fakeData
 
-def test_get_value_by_path_success(fakeData_forPath):
+def test_get_value_by_path_success(fakeData):
     path = 'a/b/c'
-    result = get_value_by_path(fakeData_forPath, path)
+    result = get_value_by_path(fetch_fake_data(fakeData), path)
     assert result == 'value'
 
-def test_get_value_by_path_failure(fakeData_forPath):
+def test_get_value_by_path_failure(fakeData):
     path = 'a.b.d'
-    result = get_value_by_path(fakeData_forPath, path)
+    result = get_value_by_path(fetch_fake_data(fakeData), path)
     assert result is None
 
-def test_get_value_by_path_empty_path(fakeData_forPath):
+def test_get_value_by_path_empty_path(fakeData):
     path = ''
-    result = get_value_by_path(fakeData_forPath, path)
+    result = get_value_by_path(fetch_fake_data(fakeData), path)
     assert result is None
 
-def test_get_value_by_path_nonexistent_key(fakeData_forPath):
+def test_get_value_by_path_nonexistent_key(fakeData):
     path = 'a/1/c'
-    result = get_value_by_path(fakeData_forPath, path)
+    result = get_value_by_path(fetch_fake_data(fakeData), path)
     assert result is None
+
+def fetch_fake_data(fakeData):
+    return fakeData("fakeData_forGetValueByPath.json")
--- test/unit/lib/service/template/functions/test_process_template.py
+++ test/unit/lib/service/template/functions/test_process_template.py
@@ -3,41 +3,21 @@ from pathlib import Path
 from unittest.mock import patch, MagicMock
 from launch.lib.service.template.functions import process_template
 from launch.enums.launchconfig import LAUNCHCONFIG_KEYS
-
-@pytest.fixture
-def mock_paths(tmp_path):
-    repo_base = tmp_path / "repo_base"
-    dest_base = tmp_path / "dest_base"
-    repo_base.mkdir()
-    dest_base.mkdir()
-    return repo_base, dest_base
-
-@pytest.fixture
-def mock_config():
-    return {
-        "dir1": {
-            "dir2": {
-                LAUNCHCONFIG_KEYS.ADDITIONAL_FILES.value: {},
-                LAUNCHCONFIG_KEYS.PROPERTIES_FILE.value: {},
-                LAUNCHCONFIG_KEYS.TEMPLATES.value: {},
-                LAUNCHCONFIG_KEYS.TEMPLATE_PROPERTIES.value: {},
-            }
-        }
-    }
+from test.conftest import fakeData_forLibServiceTemplateFunction as fakeData
 
 @patch("launch.lib.service.template.functions.LaunchConfigTemplate")
 @patch("launch.lib.service.template.functions.click.secho")
-def test_process_template_dry_run(mock_secho, MockLaunchConfigTemplate, mock_paths, mock_config):
+def test_process_template_dry_run(mock_secho, MockLaunchConfigTemplate, mock_paths, fakeData):
     repo_base, dest_base = mock_paths
-    process_template(repo_base, dest_base, mock_config, dry_run=True)
+    process_template(repo_base, dest_base, fetch_fake_data(fakeData), dry_run=True)
     mock_secho.assert_called()
     MockLaunchConfigTemplate().assert_not_called()
 
 @patch("launch.lib.service.template.functions.LaunchConfigTemplate")
 @patch("launch.lib.service.template.functions.click.secho")
-def test_process_template_create_dirs(mock_secho, MockLaunchConfigTemplate, mock_paths, mock_config):
+def test_process_template_create_dirs(mock_secho, MockLaunchConfigTemplate, mock_paths, fakeData):
     repo_base, dest_base = mock_paths
-    process_template(repo_base, dest_base, mock_config, dry_run=False)
+    process_template(repo_base, dest_base, fetch_fake_data(fakeData), dry_run=False)
     MockLaunchConfigTemplate().copy_additional_files.assert_called()
     MockLaunchConfigTemplate().properties_file.assert_called()
     MockLaunchConfigTemplate().templates.assert_called()
@@ -45,14 +25,18 @@ def test_process_template_create_dirs(mock_secho, MockLaunchConfigTemplate, mock
 
 @patch("launch.lib.service.template.functions.LaunchConfigTemplate")
 @patch("launch.lib.service.template.functions.click.secho")
-def test_process_template_skip_uuid(mock_secho, MockLaunchConfigTemplate, mock_paths, mock_config):
+def test_process_template_skip_uuid(mock_secho, MockLaunchConfigTemplate, mock_paths, fakeData):
     repo_base, dest_base = mock_paths
-    process_template(repo_base, dest_base, mock_config, skip_uuid=True, dry_run=False)
+    process_template(repo_base, dest_base, fetch_fake_data(fakeData), skip_uuid=True, dry_run=False)
     MockLaunchConfigTemplate().uuid.assert_not_called()
 
 @patch("launch.lib.service.template.functions.LaunchConfigTemplate")
 @patch("launch.lib.service.template.functions.click.secho")
-def test_process_template_include_uuid(mock_secho, MockLaunchConfigTemplate, mock_paths, mock_config):
+def test_process_template_include_uuid(mock_secho, MockLaunchConfigTemplate, mock_paths, fakeData):
     repo_base, dest_base = mock_paths
-    process_template(repo_base, dest_base, mock_config, skip_uuid=False, dry_run=False)
+    process_template(repo_base, dest_base, fetch_fake_data(fakeData), skip_uuid=False, dry_run=False)
     MockLaunchConfigTemplate().uuid.assert_called()
+
+
+def fetch_fake_data(fakeData):
+    return fakeData("fakeData_forProcessTemplate.json")
