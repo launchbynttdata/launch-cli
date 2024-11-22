5415fc9f5e21ed1af8525e3f7b175f17ec2f4226
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-11-22 08:28:25 -0600

    refactor patches and data itselfs

--- test/unit/lib/service/functions/test_common_service_workflow.py
+++ test/unit/lib/service/functions/test_common_service_workflow.py
@@ -8,130 +8,105 @@ from launch.config.launchconfig import SERVICE_REMOTE_BRANCH
 from launch.lib.service.functions import common_service_workflow
 
 
-def setup_mocks_and_params(mocker, fakedata):
-    LAUNCHCONFIG_NAME = mocker.patch(
-        "launch.constants.launchconfig.LAUNCHCONFIG_NAME", ".launch_config"
-    )
-    extract_repo_name_from_url = mocker.patch(
-        "launch.lib.service.functions.extract_repo_name_from_url",
-        return_value="repo_name",
-    )
-    clone_repository = mocker.patch("launch.lib.service.functions.clone_repository")
-    copy_template_files = mocker.patch(
-        "launch.lib.service.functions.copy_template_files"
-    )
-    process_template = mocker.patch(
-        "launch.lib.service.functions.process_template",
-        return_value={PLATFORM_SRC_DIR_PATH: "processed_path"},
-    )
-    write_text = mocker.patch("launch.lib.service.functions.write_text")
-    push_branch = mocker.patch("launch.lib.service.functions.push_branch")
-    mocker.patch("click.secho")
+def setup_patches(mocker):
+    patches = {
+        "LAUNCHCONFIG_NAME": mocker.patch(
+            "launch.constants.launchconfig.LAUNCHCONFIG_NAME", ".launch_config"
+        ),
+        "extract_repo_name_from_url": mocker.patch(
+            "launch.lib.service.functions.extract_repo_name_from_url",
+            return_value="repo_name",
+        ),
+        "clone_repository": mocker.patch(
+            "launch.lib.service.functions.clone_repository"
+        ),
+        "copy_template_files": mocker.patch(
+            "launch.lib.service.functions.copy_template_files"
+        ),
+        "process_template": mocker.patch(
+            "launch.lib.service.functions.process_template",
+            return_value={PLATFORM_SRC_DIR_PATH: "processed_path"},
+        ),
+        "write_text": mocker.patch("launch.lib.service.functions.write_text"),
+        "push_branch": mocker.patch("launch.lib.service.functions.push_branch"),
+        "secho": mocker.patch("click.secho"),
+    }
+    return patches
 
-    service_path = "service_path"
-    repository = MagicMock()
-    input_data = fakedata["workflow"]
-    git_message = "commit message"
-    skip_uuid = False
-    skip_sync = False
-    skip_git = False
-    skip_commit = False
-    dry_run = False
 
-    return (
-        LAUNCHCONFIG_NAME,
-        extract_repo_name_from_url,
-        clone_repository,
-        copy_template_files,
-        process_template,
-        write_text,
-        push_branch,
-        service_path,
-        repository,
-        input_data,
-        git_message,
-        skip_uuid,
-        skip_sync,
-        skip_git,
-        skip_commit,
-        dry_run,
-    )
+def setup_data(fakedata):
+    data = {
+        "service_path": "service_path",
+        "repository": MagicMock(),
+        "input_data": fakedata["workflow"],
+        "git_message": "commit message",
+        "skip_uuid": False,
+        "skip_sync": False,
+        "skip_git": False,
+        "skip_commit": False,
+        "dry_run": False,
+    }
+    return data
 
 
 def test_common_service_workflow(mocker, fakedata):
-    (
-        LAUNCHCONFIG_NAME,
-        extract_repo_name_from_url,
-        clone_repository,
-        copy_template_files,
-        process_template,
-        write_text,
-        push_branch,
-        service_path,
-        repository,
-        input_data,
-        git_message,
-        skip_uuid,
-        skip_sync,
-        skip_git,
-        skip_commit,
-        dry_run,
-    ) = setup_mocks_and_params(mocker, fakedata)
+    patches = setup_patches(mocker)
+    data = setup_data(fakedata)
 
     try:
         common_service_workflow(
-            service_path,
-            repository,
-            input_data,
-            git_message,
-            skip_uuid,
-            skip_sync,
-            skip_git,
-            skip_commit,
-            dry_run,
+            data["service_path"],
+            data["repository"],
+            data["input_data"],
+            data["git_message"],
+            data["skip_uuid"],
+            data["skip_sync"],
+            data["skip_git"],
+            data["skip_commit"],
+            data["dry_run"],
         )
     except KeyError as e:
         assert str(e) == "'platform'"
 
-    extract_repo_name_from_url.assert_called()
-    clone_repository.assert_any_call(
+    patches["extract_repo_name_from_url"].assert_called()
+    patches["clone_repository"].assert_any_call(
         repository_url="skeleton_url",
         target=Path(f"{BUILD_TEMP_DIR_PATH}/repo_name"),
         branch="skeleton_tag",
-        dry_run=dry_run,
+        dry_run=data["dry_run"],
     )
-    clone_repository.assert_any_call(
+    patches["clone_repository"].assert_any_call(
         repository_url="app_url",
         target=Path(f"{BUILD_TEMP_DIR_PATH}/repo_name"),
         branch="app_tag",
-        dry_run=dry_run,
+        dry_run=data["dry_run"],
     )
-    copy_template_files.assert_any_call(
+    patches["copy_template_files"].assert_any_call(
         src_dir=Path(f"{BUILD_TEMP_DIR_PATH}/repo_name"),
-        target_dir=Path(service_path),
-        dry_run=dry_run,
+        target_dir=Path(data["service_path"]),
+        dry_run=data["dry_run"],
     )
-    copy_template_files.assert_any_call(
+    patches["copy_template_files"].assert_any_call(
         src_dir=Path(f"{BUILD_TEMP_DIR_PATH}/repo_name"),
-        target_dir=Path(service_path),
+        target_dir=Path(data["service_path"]),
         not_platform=True,
-        dry_run=dry_run,
+        dry_run=data["dry_run"],
     )
-    process_template.assert_called_with(
+    patches["process_template"].assert_called_with(
         repo_base=Path.cwd(),
-        dest_base=Path(service_path),
+        dest_base=Path(data["service_path"]),
         config={PLATFORM_SRC_DIR_PATH: "platform_src_path"},
-        skip_uuid=skip_uuid,
-        dry_run=dry_run,
+        skip_uuid=data["skip_uuid"],
+        dry_run=data["dry_run"],
     )
-    write_text.assert_called_with(
-        data=input_data,
-        path=Path(f"{service_path}/{LAUNCHCONFIG_NAME}"),
-        dry_run=dry_run,
+    patches["write_text"].assert_called_with(
+        data=data["input_data"],
+        path=Path(f"{data['service_path']}/{patches['LAUNCHCONFIG_NAME']}"),
+        dry_run=data["dry_run"],
     )
-    push_branch.assert_called_with(
-        repository=repository,
+    patches["push_branch"].assert_called_with(
+        repository=data["repository"],
         branch=SERVICE_REMOTE_BRANCH,
-        commit_msg=git_message,
-        dry_run=dry_run,
+        commit_msg=data["git_message"],
+        dry_run=data["dry_run"],
     )
