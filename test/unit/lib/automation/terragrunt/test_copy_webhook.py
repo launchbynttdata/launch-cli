a577da66322a16097c583b8735e636ec198a16eb
alan-ruiseco-nttd <alanantonio.ruiseco@nttdata.com>
2024-11-25 11:42:34 -0600

    add test_copy_webhook

--- test/unit/lib/automation/terragrunt/test_copy_webhook.py
+++ test/unit/lib/automation/terragrunt/test_copy_webhook.py
@@ -1,10 +1,12 @@
 import os
 import shutil
 import subprocess
+import tempfile
 from pathlib import Path
 from unittest import mock
 
 import pytest
+from git import Repo
 
 from launch.config.terragrunt import TERRAGRUNT_RUN_DIRS
 
@@ -19,8 +21,16 @@ from launch.lib.automation.terragrunt.functions import copy_webhook
 
 
 @pytest.fixture
+def temp_directory():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        yield Path(temp_dir)
+
+
+@pytest.fixture
 def mock_clone_repository(mocker):
-    return mocker.patch("launch.lib.local_repo.repo.clone_repository")
+    return mocker.patch(
+        "launch.lib.local_repo.repo.clone_repository", return_value=None
+    )
 
 
 @pytest.fixture
@@ -48,15 +58,13 @@ def mock_shutil_copy(mocker):
     return mocker.patch("shutil.copy")
 
 
-def test_copy_webhook(
+def test_copy_webhook_dry_run(
     mock_clone_repository,
     mock_os_chdir,
     mock_os_chmod,
     mock_subprocess_run,
     mock_os_walk,
-    mock_shutil_copy,
 ):
-    # Setup
     webhooks_path = Path("/fake/webhooks_path")
     build_path = Path("/fake/build_path")
     target_environment = "dev"
@@ -74,29 +82,51 @@ def test_copy_webhook(
         )
     ]
 
+    copy_webhook(webhooks_path, build_path, target_environment, dry_run)
+
+    mock_os_chdir.assert_any_call(webhooks_path)
+    mock_subprocess_run.assert_called_once_with(
+        [webhooks_path.joinpath(WEBHOOK_BUILD_SCRIPT)], check=True
+    )
+    mock_os_chdir.assert_any_call(Path.cwd())
+
+
+def test_copy_webhook_non_dry_run(
+    mock_clone_repository,
+    mock_os_chdir,
+    mock_os_chmod,
+    mock_subprocess_run,
+    mock_os_walk,
+    mock_shutil_copy,
+    temp_directory,
+):
+    # Setup
+    webhooks_path = temp_directory
+    build_path = Path("/fake/build_path")
+    target_environment = "dev"
+    dry_run = False
+
+    mock_os_walk.return_value = [
+        (
+            str(
+                build_path.joinpath(
+                    TERRAGRUNT_RUN_DIRS["webhook"].joinpath(target_environment)
+                )
+            ),
+            [],
+            [],
+        )
+    ]
+
     # Call the function
     copy_webhook(webhooks_path, build_path, target_environment, dry_run)
 
     # Assertions
-    mock_clone_repository.assert_called_with(
-        repository_url=WEBHOOK_GIT_REPO_URL,
-        target=webhooks_path,
-        branch=WEBHOOK_GIT_REPO_TAG,
-        dry_run=dry_run,
-    )
     mock_os_chdir.assert_any_call(webhooks_path)
     mock_os_chmod.assert_called_once_with(
-        webhooks_path.joinpath(WEBHOOK_BUILD_SCRIPT),
+        webhooks_path.joinpath(WEBHOOK_BUILD_SCRIPT), 0o755
     )
     mock_subprocess_run.assert_called_once_with(
         [webhooks_path.joinpath(WEBHOOK_BUILD_SCRIPT)], check=True
     )
     mock_os_chdir.assert_any_call(Path.cwd())
-    mock_shutil_copy.assert_called_once_with(
-        webhooks_path.joinpath(WEBHOOK_ZIP),
-        str(
-            build_path.joinpath(
-                TERRAGRUNT_RUN_DIRS["webhook"].joinpath(target_environment)
-            )
-        ),
-    )
