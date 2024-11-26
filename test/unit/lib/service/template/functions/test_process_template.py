bfe7cde667c947aabb6b439d4737c41bca7b341f
Ankit Renduchintala <ankit.renduchintala@nttdata.com>
2024-11-26 11:58:37 -0500

    Patch/269 adding test case (#58)
    * adding tests for process_template function
    
    * removing unwanted import
    
    * adding test cases for process_template function
    
    * refactoring test data to json files and updating test functions

--- test/unit/lib/service/template/functions/test_process_template.py
+++ test/unit/lib/service/template/functions/test_process_template.py
@@ -1,6 +1,42 @@
 import pytest
+from pathlib import Path
+from unittest.mock import patch, MagicMock
+from launch.lib.service.template.functions import process_template
+from launch.enums.launchconfig import LAUNCHCONFIG_KEYS
+from test.conftest import fakeData_forLibServiceTemplateFunction as fakeData
+
+@patch("launch.lib.service.template.functions.LaunchConfigTemplate")
+@patch("launch.lib.service.template.functions.click.secho")
+def test_process_template_dry_run(mock_secho, MockLaunchConfigTemplate, mock_paths, fakeData):
+    repo_base, dest_base = mock_paths
+    process_template(repo_base, dest_base, fetch_fake_data(fakeData), dry_run=True)
+    mock_secho.assert_called()
+    MockLaunchConfigTemplate().assert_not_called()
+
+@patch("launch.lib.service.template.functions.LaunchConfigTemplate")
+@patch("launch.lib.service.template.functions.click.secho")
+def test_process_template_create_dirs(mock_secho, MockLaunchConfigTemplate, mock_paths, fakeData):
+    repo_base, dest_base = mock_paths
+    process_template(repo_base, dest_base, fetch_fake_data(fakeData), dry_run=False)
+    MockLaunchConfigTemplate().copy_additional_files.assert_called()
+    MockLaunchConfigTemplate().properties_file.assert_called()
+    MockLaunchConfigTemplate().templates.assert_called()
+    MockLaunchConfigTemplate().template_properties.assert_called()
+
+@patch("launch.lib.service.template.functions.LaunchConfigTemplate")
+@patch("launch.lib.service.template.functions.click.secho")
+def test_process_template_skip_uuid(mock_secho, MockLaunchConfigTemplate, mock_paths, fakeData):
+    repo_base, dest_base = mock_paths
+    process_template(repo_base, dest_base, fetch_fake_data(fakeData), skip_uuid=True, dry_run=False)
+    MockLaunchConfigTemplate().uuid.assert_not_called()
+
+@patch("launch.lib.service.template.functions.LaunchConfigTemplate")
+@patch("launch.lib.service.template.functions.click.secho")
+def test_process_template_include_uuid(mock_secho, MockLaunchConfigTemplate, mock_paths, fakeData):
+    repo_base, dest_base = mock_paths
+    process_template(repo_base, dest_base, fetch_fake_data(fakeData), skip_uuid=False, dry_run=False)
+    MockLaunchConfigTemplate().uuid.assert_called()
 
 
-@pytest.mark.skip("TODO: Ticket: tbd XXXXX")
-def test_process_template():
-    pass
+def fetch_fake_data(fakeData):
+    return fakeData("fakeData_forProcessTemplate.json")
